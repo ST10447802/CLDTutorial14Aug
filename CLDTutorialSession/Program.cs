016891d36437ac51e16d5aa5786108eeb11fd46d
ST10447802-SIYANDA-MHLONGO <163291337+ST10447802@users.noreply.github.com>
2026-08-14 11:07:24 +0200

    Third Commit

--- CLDTutorialSession/Program.cs
+++ CLDTutorialSession/Program.cs
@@ -5,7 +5,11 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World");
+<<<<<<< Updated upstream
             Console.WriteLine("This is a C# console application");
+=======
+            Console.WriteLine("This is a gimmick");
+>>>>>>> Stashed changes
             Console.WriteLine("Cloud tutorial ");
         }
     }
