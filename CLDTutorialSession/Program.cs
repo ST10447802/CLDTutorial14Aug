5f58043f2c972be34fec9c7ae7b4563c3535c2bf
ST10447802-SIYANDA-MHLONGO <163291337+ST10447802@users.noreply.github.com>
2026-08-14 10:48:06 +0200

    TutorialPush

---
+++ CLDTutorialSession/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace CLDTutorialSession
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            Console.WriteLine("This is a C# console application");
+            Console.WriteLine("Cloud tutorial ");
+        }
+    }
+}
